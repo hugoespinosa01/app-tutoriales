20cd5cc5824ed09e42d1aeb06a5e1915d2e28805
borahe14 <deniss.iba13@gmail.com>
2024-01-14 18:49:40 -0500

    Add font

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useFonts } from 'expo-font';
 export default function App() {
+  const [fontsLoaded] = useFonts({ 
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),  
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-Semibold.ttf'),
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'outfit-bold'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
