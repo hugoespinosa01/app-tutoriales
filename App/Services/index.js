6dcadcf29340063acff601d20b76ec565932c59f
Sebastian Pazos <apazos@ec.krugercorp.com>
2024-01-18 21:54:43 -0500

    Agregacion de nuevos query

--- App/Services/index.js
+++ App/Services/index.js
@@ -83,3 +83,65 @@ export const getUserEnrolledCourse = async (courseId, userEmail) => {
   const result = await request(MASTER_URL, query);
   return result;
 };
+
+export const MarkChapterCompleted = async (chapterId,recordId) => {
+  const mutationQuery = gql`
+  mutation markChapterCompleted {
+    updateUserEnrolledCourse(
+      data: {completedChapter: {create: {data: {chapterId: "`+chapterId+`"}}}}
+      where: {id: "`+recordId+`"}
+    ) {
+      id
+    }
+    publishManyUserEnrolledCoursesConnection {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+  `
+  const result = await request(MASTER_URL, mutationQuery);
+  return result;
+}
+
+export const createNewUser = async (userName, email, profileImageUrl) => {
+  const mutationQuery = gql`
+    mutation CreateNewUser {
+      upsetUserDetail(
+        upsert: {create:
+          {email: "`+email+`",
+          point: 10,
+        profileImage: "`+profileImageUrl+`",
+        userName: "`+userName+`"},
+        update: {email: "`+email+`",
+        profileImage: 
+        "`+profileImageUrl+`", userName: "abc"}}
+      where: {email: "`+email+`"}
+    ) {
+      id
+    }
+    publishUserDetail(where: {email: "`+email+`"}) {
+      id
+    }
+  }
+  `;
+
+  const result = await request(MASTER_URL, mutationQuery);
+  return result;
+};
+
+export const getUserDetail = async (email) => {
+  const query = gql`
+    query GetUserDetail {
+      userDetail(where:
+         { email: "`+email+`" }) { 
+        point
+      }
+    }
+  `;
+  const result = await request(MASTER_URL, query);
+  return result.userDetail;
+}
+
